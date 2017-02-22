538b46ee59d08a2553cd4bde71029a20166af0db
葛尧 <geyao2012@gmail.com>
2017-02-23 00:37:51 +0800

    完成了打分系统的数据库的设计

---
+++ src/main/java/pojo/valueObject/assist/ProjectAccessTypeVO.java
@@ -0,0 +1,49 @@
+package pojo.valueObject.assist;
+
+import pojo.valueObject.domain.ProjectVO;
+
+import javax.persistence.*;
+
+/**
+ * Created by geyao on 2017/2/23.
+ */
+@Entity
+@Table(name = "project_accessType")
+public class ProjectAccessTypeVO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(targetEntity = ProjectVO.class)
+    @JoinColumn(name = "projectId", referencedColumnName = "id")
+    private ProjectVO projectVO;
+
+    private String type;
+
+    public ProjectAccessTypeVO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ProjectVO getProjectVO() {
+        return projectVO;
+    }
+
+    public void setProjectVO(ProjectVO projectVO) {
+        this.projectVO = projectVO;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/pojo/valueObject/assist/StudentScoreVO.java
@@ -0,0 +1,68 @@
+package pojo.valueObject.assist;
+
+import pojo.valueObject.domain.ProjectVO;
+import pojo.valueObject.domain.StudentVO;
+
+import javax.persistence.*;
+
+/**
+ * Created by geyao on 2017/2/23.
+ */
+@Entity
+@Table(name = "student_score_record")
+public class StudentScoreVO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+//    @ManyToOne(targetEntity = ProjectVO.class)
+//    @JoinColumn( name = "projectId", referencedColumnName = "id")
+//    private ProjectVO projectVO;
+
+    @ManyToOne(targetEntity = ProjectAccessTypeVO.class)
+    @JoinColumn(name = "typeId", referencedColumnName = "id")
+    private ProjectAccessTypeVO projectAccessTypeVO;
+
+    @ManyToOne(targetEntity = StudentVO.class)
+    @JoinColumn(name = "studentId", referencedColumnName = "id")
+    private StudentVO studentVO;
+
+    private Integer score;
+
+    public StudentScoreVO() {
+        super();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public ProjectAccessTypeVO getProjectAccessTypeVO() {
+        return projectAccessTypeVO;
+    }
+
+    public void setProjectAccessTypeVO(ProjectAccessTypeVO projectAccessTypeVO) {
+        this.projectAccessTypeVO = projectAccessTypeVO;
+    }
+
+    public StudentVO getStudentVO() {
+        return studentVO;
+    }
+
+    public void setStudentVO(StudentVO studentVO) {
+        this.studentVO = studentVO;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    private
+}
