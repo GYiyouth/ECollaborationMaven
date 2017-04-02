a1d8498b886de6d6cc915ca51c5c9f66b3674ede
葛尧 <geyao2012@gmail.com>
2017-04-02 17:16:34 +0800

    改写了PlanDAO，相关的BO，测试了查询的action

--- src/main/java/pojo/businessObject/PlanBO.java
+++ src/main/java/pojo/businessObject/PlanBO.java
@@ -1,6 +1,7 @@
 package pojo.businessObject;
 
 import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import pojo.DAO.PlanDAO;
 import pojo.DAO.ProjectDAO;
 import pojo.valueObject.DTO.PlanDTO;
@@ -17,6 +18,8 @@ import java.util.Map;
  * Created by geyao on 2017/3/1.
  */
 public class PlanBO {
+    @Autowired
+    private PlanDAO planDAO;
     /**
      * 新增计划,并往session里添加planVO与projectVO
      * @param planVO
@@ -30,7 +33,7 @@ public class PlanBO {
         if (planVO == null || projectId == null)
             return jsonObject;
         try {
-            PlanDAO planDAO = BeanFactory.getBean("planDAO", PlanDAO.class);
+//            PlanDAO planDAO = BeanFactory.getBean("planDAO", PlanDAO.class);
             ProjectDAO projectDAO = BeanFactory.getBean("projectDAO", ProjectDAO.class);
             ProjectVO projectVO = projectDAO.getProjectVO(projectId);
             planVO = planDAO.addPlan(planVO, projectVO);
@@ -65,7 +68,7 @@ public class PlanBO {
         ArrayList<PlanDTO> planDTOList = new ArrayList<>();
         ArrayList<PlanVO> planVOList = new ArrayList<>();
         try {
-            PlanDAO planDAO = new PlanDAO();
+//            PlanDAO planDAO = new PlanDAO();
             planVOList = planDAO.getPlanVOList(studentVO, projectId);
             if (session.containsKey("planVOList"))
                 session.remove("planVOList");
